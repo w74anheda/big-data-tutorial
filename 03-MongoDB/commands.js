69c7b4b10d14382188921c3d0741ecb6face3c42
masoud nazarpoor <36822055+masoudnazarpoor@users.noreply.github.com>
2023-05-10 00:13:50 +0330

    update

--- 03-MongoDB/commands.js
+++ 03-MongoDB/commands.js
@@ -1,12 +1,10 @@
 db.users.aggregate([
-    { $lookup: { from: "orders", localField: "_id", foreignField: "user_id", as: "user_orders" } },
-    { $unwind: "$user_orders" },
-    { $lookup: { from: "product", localField: "user_orders.product_id", foreignField: "_id", as: "user_orders.product" } },
     {
-        $group: {
-            _id: "$_id",
-            name: { $first: "$name" },
-            orders: { $push: { "_id": "$user_orders._id", "product_id": "$user_orders.product_id", "quantity": "$user_orders.quantity" } }
+        $lookup: {
+            from: "orders",
+            localField: "orders",
+            foreignField: "_id",
+            as: "user_orders"
         }
     }
 ]).pretty()
