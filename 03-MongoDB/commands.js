22695de933414c87bdee67cc9b290a0157e286fe
masoud nazarpoor <36822055+masoudnazarpoor@users.noreply.github.com>
2023-05-14 23:28:18 +0330

    update

--- 03-MongoDB/commands.js
+++ 03-MongoDB/commands.js
@@ -1,4 +1,24 @@
-db.find(
-    { $text: { $search: "red car" } },
-    { score: { $meta: "text-score" } }
-).sort({ score: { $meta: "textScore" } })
+db.places.insertOne(
+    {
+        name: "Imam Khomeyni Hospital",
+        point: {
+            type: "Point",
+            coordinates: [48.68601619690574, 31.32995237098178] // long/lat
+        }
+    }
+)
+
+db.places.find({
+    point: {
+        $near: {
+            $geometry: {
+                type: "Point",
+                coordinates: [48.68701670610951, 31.328589484869]
+            },
+            $maxDistance: 180, //meter
+            $minDistance: 10
+        }
+    }
+})
+
+db.places.createIndex({ point: "2dsphere" })
